c0679e402cd7fd3e83343f7d579056c854ef61d4
Yassine Ezzar <yassine.ezzar@esprit.tn>
2024-03-03 14:31:55 +0100

    push

--- lib/screens/Notifications.dart
+++ lib/screens/Notifications.dart
@@ -1,61 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:teamsyncai/providers/ChangeNotifier.dart';
 
-class Notifications extends StatefulWidget {
+class Notifications extends StatelessWidget {
   @override
-  _NotifsState createState() => _NotifsState();
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => NotificationSettings(),
+      child: NotificationsPage(),
+    );
+  }
 }
 
-class _NotifsState extends State<Notifications>{
-bool _pushNotifications = true; // Initial state for push notifications
-  bool _emailNotifications = false; // Initial state for email notifications
-  bool _locationServices = false; // Initial state for location services
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
 
+class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Notification Settings'),
-        backgroundColor: Colors.orange,
-          elevation: 15, 
-          iconTheme: const IconThemeData(color: Colors.black38),
       ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                const Text(
-                  'Choose what notifications you want to receive below and we will update the settings.',
-                  style: TextStyle(fontSize: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Consumer<NotificationSettings>(
+                builder: (context, settings, child) => _buildNotificationTile(
+                  title: 'Push Notifications',
+                  subtitle: 'Receive occasional push notifications from our application.',
+                  value: settings.pushNotifications,
+                  onChanged: (value) => settings.setPushNotifications(value),
                 ),
-                SizedBox(height: 20.0),
-                SwitchListTile(
-                  title: const Text('Push Notifications'),
-                  subtitle: Text(
-                      'Receive occasional push notifications from our application.'),
-                  value: _pushNotifications,
-                  onChanged: (value) => setState(() => _pushNotifications = value),
+              ),
+              Consumer<NotificationSettings>(
+                builder: (context, settings, child) => _buildNotificationTile(
+                  title: 'Email Notifications',
+                  subtitle: 'Receive email notifications from our marketing team about new features.',
+                  value: settings.emailNotifications,
+                  onChanged: (value) => settings.setEmailNotifications(value),
                 ),
-                SwitchListTile(
-                  title: const Text('Phone Notifications'),
-                  subtitle: const Text(
-                      'Receive email notifications from our marketing team about new features.'),
-                  value: _emailNotifications,
-                  onChanged: (value) => setState(() => _emailNotifications = value),
+              ),
+              Consumer<NotificationSettings>(
+                builder: (context, settings, child) => _buildNotificationTile(
+                  title: 'Location Services',
+                  subtitle: 'Allow us to track your location to keep track of spending and keep you safe (optional).',
+                  value: settings.locationServices,
+                  onChanged: (value) => settings.setLocationServices(value),
                 ),
-                SwitchListTile(
-                  title: const Text('Location Services'),
-                  subtitle: const Text(
-                      'Allow us to track your location to keep track of spending and keep you safe (optional).'),
-                  value: _locationServices,
-                  onChanged: (value) => setState(() => _locationServices = value),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
-} 
+
+  Widget _buildNotificationTile({
+    required String title,
+    required String subtitle,
+    required bool value,
+    required ValueChanged<bool> onChanged,
+  }) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: ListTile(
+        title: Text(
+          title,
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: TextStyle(fontSize: 14.0, color: Colors.black54),
+        ),
+        trailing: Switch(
+          value: value,
+          onChanged: onChanged,
+          activeColor: Colors.orange,
+        ),
+      ),
+    );
+  }
+}
