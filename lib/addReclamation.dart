aef972b7f4a235d6a19cdf4f7f69aeb6a5aa7cd9
Ekbel <zrellliekbel@gmail.com$>
2024-03-12 14:47:03 +0100

    addReclamation

--- lib/addReclamation.dart
+++ lib/addReclamation.dart
@@ -1,1 +1,61 @@
 import 'package:flutter/material.dart';
+class AddReclamation extends StatefulWidget {
+  @override
+  _AddReclamationState createState() => _AddReclamationState();
+}
+
+class _AddReclamationState extends State<AddReclamation> {
+  final _formKey = GlobalKey<FormState>();
+  final _titreController = TextEditingController();
+  final _descriptionController = TextEditingController();
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add reclamation'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titreController,
+                decoration: InputDecoration(labelText: 'Titre'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer un titre';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 5,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer une description';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // 
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text('Ajouter'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
