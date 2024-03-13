aff7ab33e358fc2c2ff6d5e5c70867176322fefd
AhmedMaadi <149616696+AhmedMaadi@users.noreply.github.com>
2024-03-13 01:22:54 +0100

    consummation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_pim/modulesList.dart';
 import 'package:flutter_pim/tasksPage.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -9,105 +11,68 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  Future<List<Map<String, dynamic>>> fetchModules() async {
+    final response = await http.get(Uri.parse(
+        'http://192.168.231.1:3000/modules/project/65f0e8e8d5879428b4d0f80b'));
+    if (response.statusCode == 200) {
+      return List<Map<String, dynamic>>.from(
+          jsonDecode(response.body)['modules']);
+    } else {
+      throw Exception('Failed to load modules');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Builder(
-        builder: (BuildContext context) {
-          return Scaffold(
-            body: SafeArea(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const Row(
-                      children: [
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 10.0),
-                            child: CircleAvatar(
-                              radius: 30.0,
-                              backgroundImage: AssetImage('images/zz.png'),
-                            ),
-                          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Modules'),
+        ),
+        body: FutureBuilder<List<Map<String, dynamic>>>(
+          future: fetchModules(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              List<Map<String, dynamic>> modules = snapshot.data!;
+              return ListView.builder(
+                itemCount: modules.length,
+                itemBuilder: (context, index) {
+                  var module = modules[index];
+                  return ListTileModule(
+                    projectTitle: module['projectID'],
+                    moduleTitle: module['module_name'],
+                    profileImagePaths: const ['images/reclamation.jpg'],
+                    percentage:
+                        50, // You can modify this to get the percentage from the module data
+                    imagePath: 'images/pp.png',
+                    task: Task(
+                      taskTitle: 'Design Template Screens',
+                      taskDescription:
+                          'Create template screen for tasks todo app.',
+                      date: DateTime.now(),
+                      members: [
+                        Member(
+                          name: 'ahmed maadi',
+                          email: 'ahmed@example.com',
+                          profileImagePath: ['images/reclamation.jpg'],
                         ),
-                        SizedBox(width: 10.0),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(top: 10.0),
-                              child: Text(
-                                'Ahmed Maadi',
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            Text(
-                              'Good Morning Ahmed !',
-                              style: TextStyle(
-                                fontSize: 12.0,
-                              ),
-                            ),
-                          ],
+                        Member(
+                          name: 'hama boualeg',
+                          email: 'hama@example.com',
+                          profileImagePath: ['images/zz.png'],
                         ),
                       ],
                     ),
-                    const SizedBox(height: 20.0),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        right: 30.0,
-                        left: 30.0,
-                        top: 8.0,
-                      ),
-                      child: Material(
-                        elevation: 3,
-                        shadowColor: Colors.orange,
-                        borderRadius: BorderRadius.circular(30.0),
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Find your module...',
-                            prefixIcon: Icon(Icons.search),
-                            contentPadding:
-                                EdgeInsets.symmetric(vertical: 10.0),
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    ListTileModule(
-                      title: 'payement',
-                      description: 'payement description',
-                      profileImagePaths: const ['images/zz.png'],
-                      percentage: 50,
-                      imagePath: 'images/pp.png',
-                      task: Task(
-                        taskTitle: 'Design Template Screens',
-                        taskDescription:
-                            'Create template screen for tasks todo app.',
-                        date: DateTime.now(),
-                        members: [
-                          Member(
-                            name: 'ahmed maadi',
-                            email: 'ahmed@example.com',
-                            profileImagePath: ['images/zz.png'],
-                          ),
-                          Member(
-                            name: 'hama boualeg',
-                            email: 'hama@example.com',
-                            profileImagePath: ['images/zz.png'],
-                          ),
-                        ],
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => TasksPage(
-                                task: Task(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => TasksPage(
+                            task: Task(
                               taskTitle: 'Design Template Screens',
                               taskDescription:
                                   'Create template screen for tasks todo app.',
@@ -124,66 +89,17 @@ class MyApp extends StatelessWidget {
                                   profileImagePath: ['images/zz.png'],
                                 ),
                               ],
-                            )),
+                            ),
                           ),
-                        );
-                      },
-                    ),
-                    ListTileModule(
-                      title: 'Reclamation ',
-                      description: 'Reclamation description',
-                      profileImagePaths: const ['images/zz.png'],
-                      percentage: 50,
-                      imagePath: 'images/reclamation.jpg',
-                      task: Task(
-                        taskTitle: 'UI/UX screens',
-                        taskDescription: 'Create UI/UX screens todo app.',
-                        date: DateTime.now(),
-                        members: [
-                          Member(
-                            name: 'ahmed maadi',
-                            email: 'ahmed@example.com',
-                            profileImagePath: ['images/zz.png'],
-                          ),
-                          Member(
-                            name: 'ekbel zrelli',
-                            email: 'ahmed@example.com',
-                            profileImagePath: ['images/zz.png'],
-                          ),
-                        ],
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => TasksPage(
-                                task: Task(
-                              taskTitle: 'UI/UX screens',
-                              taskDescription: 'Create UI/UX screens todo app.',
-                              date: DateTime.now(),
-                              members: [
-                                Member(
-                                  name: 'ahmed maadi',
-                                  email: 'ahmed@example.com',
-                                  profileImagePath: ['images/zz.png'],
-                                ),
-                                Member(
-                                  name: 'ekbel zrelli',
-                                  email: 'ahmed@example.com',
-                                  profileImagePath: ['images/zz.png'],
-                                ),
-                              ],
-                            )),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
--- lib/modulesList.dart
+++ lib/modulesList.dart
@@ -1,27 +1,26 @@
-// ignore: file_names
-// ignore_for_file: file_names, duplicate_ignore
+// ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
 import 'package:flutter_pim/tasksPage.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class ListTileModule extends StatelessWidget {
-  final String title;
-  final String description;
+  final String projectTitle;
+  final String moduleTitle;
   final List<String> profileImagePaths;
   final int percentage;
-  final String imagePath; 
-  final Task task; // Add Task property
-  final VoidCallback onTap;// New property for image asset path
+  final String imagePath;
+  final Task task;
+  final VoidCallback onTap;
 
   const ListTileModule({
     Key? key,
-    required this.title,
-    required this.description,
+    required this.projectTitle,
+    required this.moduleTitle,
     required this.profileImagePaths,
     required this.percentage,
     required this.imagePath,
-     required this.task,
+    required this.task,
     required this.onTap,
   }) : super(key: key);
 
@@ -30,35 +29,34 @@ class ListTileModule extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      child: Card(
-        elevation: 5,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Image
-            Container(
-              height: 150,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10.0),
-                  topRight: Radius.circular(10.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Card(
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 150,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    topRight: Radius.circular(10.0),
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage(imagePath),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                image: DecorationImage(
-                  image: AssetImage(imagePath),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Column(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(left: 20.0, top: 40),
                       child: Text(
-                        title,
+                        projectTitle,
                         style: const TextStyle(
                           color: Colors.white,
                           fontSize: 20,
@@ -69,63 +67,61 @@ class ListTileModule extends StatelessWidget {
                     Padding(
                       padding: const EdgeInsets.only(left: 20.0, top: 10),
                       child: Text(
-                        description,
+                        moduleTitle,
                         style: const TextStyle(
                           color: Colors.white,
                           fontSize: 15,
                         ),
                       ),
                     ),
-                  ]),
-            ),
-            // Title, Description, Icon, and Percentage
-            Container(
-              height: 100,
-              padding: const EdgeInsets.all(10.0),
-              color: Colors.white,
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: List.generate(
-                          profileImagePaths.length,
-                          (index) => Padding(
-                            padding: const EdgeInsets.only(left: 20.0),
-                            child: CircleAvatar(
-                              radius: 15.0,
-                              backgroundImage:
-                                  AssetImage(profileImagePaths[index]),
+                  ],
+                ),
+              ),
+              Container(
+                height: 100,
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: List.generate(
+                            profileImagePaths.length,
+                            (index) => Padding(
+                              padding: const EdgeInsets.only(left: 20.0),
+                              child: CircleAvatar(
+                                radius: 15.0,
+                                backgroundImage: AssetImage(profileImagePaths[index]),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Text(
-                        '$percentage%', // Sum of all percentages
-                        style: const TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
+                        Text(
+                          '$percentage%',
+                          style: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  // Integrated Pie Chart
-                  LinearPercentIndicator(
-                    width: MediaQuery.of(context).size.width - 80,
-                    lineHeight: 15.0,
-                    percent: percentage / 100,
-                    backgroundColor: const Color.fromARGB(255, 238, 225, 225),
-                    progressColor: Colors.orange,
-                  ),
-                ],
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    LinearPercentIndicator(
+                      width: MediaQuery.of(context).size.width - 80,
+                      lineHeight: 15.0,
+                      percent: percentage / 100,
+                      backgroundColor: const Color.fromARGB(255, 238, 225, 225),
+                      progressColor: Colors.orange,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    ),
     );
   }
 }
